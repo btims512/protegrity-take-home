ed1284fbdf606de9c4afb3c47be07a6150ac7ef8
Ben <71402472+btims512@users.noreply.github.com>
2023-03-26 20:16:43 -0600

    1st pre-deploy push

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -13,8 +13,7 @@ const Body = () => {
     fetch(`https://www.reddit.com/r/data.json?count=${(currentPage - 1) * 12}&limit=96`)
       .then((response) => response.json())
       .then((data) => {
-        setPosts(data.data.children
-          .slice((currentPage - 1) * 12, currentPage * 12)
+        setPosts(data.data.children.slice((currentPage - 1) * 12, currentPage * 12)
           .map((post) => ({
             title: post.data.title,
             num_comments: post.data.num_comments,
@@ -27,10 +26,13 @@ const Body = () => {
   }, [currentPage]);
 
   const handleNextClick = () => {
-    if (currentPage < 12) {
-      setCurrentPage(currentPage + 1);
+  if (currentPage < 8) {
+    setCurrentPage(currentPage + 1);
+    if (window.innerWidth <= 767) {
+      window.scrollTo(0, 0);
     }
-  };
+  }
+};
 
   const handlePrevClick = () => {
     if (currentPage > 1) {
@@ -38,65 +40,59 @@ const Body = () => {
     }
   };
 
-  const truncateTitle = (title) => {
-    if (title.length > 120) {
-      return title.slice(0, 120) + '...';
-    } else {
-      return title;
-    }
-  };
-
   return (
-    <>
-      <div className='header'>
-        <div className='avatar-container'>
-          <img src={Avatar} width="88" alt="" />
-        </div>
-        <div className="flex-grid-header">
+  <>
+    <div className='header'>
+      <div className='avatar-container'>
+        <img src={Avatar} width="88" alt="" />
+      </div>
+      <div className="flex-grid-header">
         <div className="title-container">
-        <div className='title'>
-          <h1>r/data</h1>
-        </div>
-        <p>Welcome to the Protegrity page that features a collection of posts from r/data, a subreddit dedicated to discussing and sharing data and datasets.</p>
-        </div>
+          <div className='title'>
+            <h1>r/data</h1>
+          </div>
+          <p>Welcome to the Protegrity page that features a collection of posts from r/data, a subreddit dedicated to discussing and sharing data and datasets.</p>
         </div>
       </div>
-      {isLoading ? (
-        <div>
-          <div className="loading">
-            <img src={Spinner} alt="loading..." />
-          </div>
+    </div>
+    {isLoading ? (
+      <div>
+        <div className="loading">
+          <img src={Spinner} alt="loading..." />
         </div>
-      ) : (
-        <div className="flex-grid">
-          <div className="grid-container">
-            {posts.map((post, index) => (
-              <div className="grid-item" key={index}>
-                <div className="row-top">
-                  <h3>{(post.title)}</h3>
-                  <p>u/{post.author}</p>
-                </div>
-                <div className="row-bottom">
-                  <p>{post.num_comments} Comments</p>
-                  <a href={post.url}><button className="global-button"><h4>View Post</h4></button></a>
-                </div>
+      </div>
+    ) : (
+      <div className="flex-grid">
+        <div className="grid-container">
+          {posts.map((post, index) => (
+            <div className="grid-item" key={index}>
+              <div className="row-top">
+                <h3>{(post.title)}</h3>
+                <p>u/{post.author}</p>
               </div>
-            ))}
-          </div>
+              <div className="row-bottom">
+                <p>{post.num_comments} Comments</p>
+                <a href={post.url}><button className="global-button"><h4>View Post</h4></button></a>
+              </div>
+            </div>
+          ))}
         </div>
-      )}
-
-<div>
-
+      </div>
+    )}
+    <div>
       <div className="flex-grid-bottom">
-      <div className="button-container">
-        <button className="global-button" onClick={handlePrevClick}><h4>Prev</h4></button>
-        <h4>{currentPage}-12 of {Math.min(currentPage+11, 8)}</h4>
-        <button className="global-button" onClick={handleNextClick}><h4>Next</h4></button>
+        <div className="button-container">
+          <button className="global-button prev-button" onClick={handlePrevClick} style={currentPage === 1 ? {background: "#2B3037", opacity: 0.3, cursor: "not-allowed"} : null}>
+            <h4 style={currentPage === 1 ? {color: "white"} : null}>Prev</h4>
+          </button>
+          <h4>page {currentPage} of {Math.min(currentPage+11, 8)}</h4>
+          <button className="global-button next-button" style={currentPage === 8 ? {background: "#2B3037", opacity: 0.3, cursor: "not-allowed"} : null} onClick={handleNextClick}>
+            <h4>Next</h4>
+          </button>
+        </div>
       </div>
-      </div>
-</div>
-    </>
+    </div>
+  </>
   );
 };
 
